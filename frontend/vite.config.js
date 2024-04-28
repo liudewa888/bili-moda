d25938b43f52de4bf1531697dc44aa4815a333ff
liudewa888 <2549711475@qq.com>
2024-04-28 20:20:57 +0800

    fix: delete  sensitive info

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,30 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 import ElementPlus from "unplugin-element-plus/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
-  plugins: [vue(),ElementPlus()],
-  optimizeDeps: {
-  },
-  server:{
-    host:'0.0.0.0',
+  base: "./",
+  plugins: [vue(), ElementPlus()],
+  optimizeDeps: {},
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
     proxy: {
-      "/api": {
-        // 选项写法
-        target: "http://127.0.0.1:9080",
-        changeOrigin: true,
-        rewrite: (path) => {
-          path = path.replace(/^\/api/, "");
-          // 进行调试输出
-          console.log('vite proxy url: ',path);
-          return path;
-        },
-      },
       // "/api": {
       //   // 选项写法
-      //   target: "http://www.yztpsg.cn/moda/",
+      //   target: "http://127.0.0.1:9080",
       //   changeOrigin: true,
       //   rewrite: (path) => {
       //     path = path.replace(/^\/api/, "");
@@ -33,6 +22,17 @@ export default defineConfig({
       //     return path;
       //   },
       // },
-    }
-  }
-})
+      "/api": {
+        // 选项写法
+        target: "http://www.yztpsg.cn/moda/",
+        changeOrigin: true,
+        rewrite: (path) => {
+          path = path.replace(/^\/api/, "");
+          // 进行调试输出
+          console.log("vite proxy url: ", path);
+          return path;
+        },
+      },
+    },
+  },
+});
